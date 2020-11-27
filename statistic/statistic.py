5e010d863739519b27073ddee371385185b50925
flamingm321 <flamingm321@gmail.com>
2020-11-27 19:17:13 +0800

    2020-11-27 -- add season mean mehtod

--- statistic/statistic.py
+++ statistic/statistic.py
@@ -39,6 +39,7 @@ class Timean(object):
     storageForward: False ->
         storage value in endtime
     '''
+
     def __init__(self):
         self.set_config(init=True)
 
@@ -63,26 +64,55 @@ class Timean(object):
     # def _fixTime(time,data,timeStep:dict,ratio:int,zeroPara:dict,storageForward:bool,starttime:datetime.datetime=None,endtime:datetime.datetime=None):
         '''
         zeroPara: set start datetime para
+        season enum:
+            1: spring
+            2: summer
+            3: autumn
+            4: winter
         '''
-        dt = relativedelta(**timeStep)
-        if not storageForward: time+=dt; time+=datetime.timedelta(microseconds=-1)
         minTime = np.nanmin(time) if starttime==None else starttime
         maxTime = np.nanmax(time) if endtime==None else endtime
-        maxTime+=dt
-        if zeroPara!=None: minTime=minTime.replace(**zeroPara)
-        # if ratio==None: ratio=0
+        if 'season' in timeStep.keys():
+            dt = relativedelta(months=3)
+            if not storageForward: time+=dt; time+=datetime.timedelta(microseconds=-1)
+            maxTime+=dt
+            if zeroPara!=None: minTime=minTime.replace(**zeroPara)
+            dummy = []
+            tummy = []
+            count = []
 
-        dummy = []
-        tummy = []
-        count = []
-        while minTime<=maxTime:
-            mask = np.where((time>=minTime) & (time<minTime+dt))[0]
-            if mask.size==0: minTime+=dt; continue
-            tummy.append(minTime)
-            count.append(np.sum(np.isfinite(data[mask])))
-            dummy.append(np.nanmean(data[mask],axis=0))
-            # dummy.append(np.nanmean(data[mask],axis=0) if count[-1]>=ratio else np.array([np.nan]*len(data[0])))
-            minTime+=dt
+            # deal with perfix date before a new start
+            i = Timean._get_season(minTime.month)
+            year = minTime.year if minTime.month!=12 else minTime.year+1
+            mask=np.where(time<datetime.datetime(year,3*i,1))[0]
+            t,d,c = Timean._nofixTime(time[mask],data[mask],parameter='season')
+            tummy+=list(t); dummy+=(d); count+=list(c)
+            minTime=datetime.datetime(year,3*i,1)
+
+            while minTime<=maxTime:
+                if minTime>max(time): break
+                mask=np.where((time>=minTime) & (time<minTime+dt))[0]
+                t,d,c = Timean._nofixTime(time[mask],data[mask],parameter='season')
+                tummy+=list(t); dummy+=(d); count+=list(c)
+                minTime+=dt
+        else:
+            dt = relativedelta(**timeStep)
+            if not storageForward: time+=dt; time+=datetime.timedelta(microseconds=-1)
+            maxTime+=dt
+            if zeroPara!=None: minTime=minTime.replace(**zeroPara)
+            # if ratio==None: ratio=0
+
+            dummy = []
+            tummy = []
+            count = []
+            while minTime<=maxTime:
+                mask = np.where((time>=minTime) & (time<minTime+dt))[0]
+                if mask.size==0: minTime+=dt; continue
+                tummy.append(minTime)
+                count.append(np.sum(np.isfinite(data[mask])))
+                dummy.append(np.nanmean(data[mask],axis=0))
+                # dummy.append(np.nanmean(data[mask],axis=0) if count[-1]>=ratio else np.array([np.nan]*len(data[0])))
+                minTime+=dt
         return tummy,dummy,count
 
     @staticmethod
@@ -90,8 +120,24 @@ class Timean(object):
     # def _nofixTime(time,data,parameter:str,ratio:int):
         '''
         parameter: set the datetime parameter (second, minute ...etc) will be used to calculate
+        season enum:
+            1: winter
+            2: spring
+            3: summer
+            4: autumn
         '''
-        time_para_list = [eval(f"val.{parameter}") for val in time]
+        season_dict = {
+            1: 'Winter',
+            2: 'Spring',
+            3: 'Summer',
+            4: 'Autumn',
+        }
+        if parameter.lower()=='season':
+            time_para_list = [Timean._get_season(val.month) for val in time]
+        else:
+            time_para_list = [eval(f"val.{parameter}") for val in time]
+        time_para_list = np.array(time_para_list)
+        if time_para_list.size==0: return np.array(np.nan),np.array(np.nan),np.array(np.nan)
         minTime = np.nanmin(time_para_list)
         maxTime = np.nanmax(time_para_list)
         # if ratio==None: ratio=0
@@ -100,12 +146,23 @@ class Timean(object):
         tummy = []
         count = []
         for i in range(minTime,maxTime+1):
-            mask = np.where(eval(f"time.{parameter}")==i)[0]
-            tummy.append(i)
+            mask = np.where(time_para_list==i)[0]
+            tummy.append(i if parameter.lower()!='season' else [time[mask[0]].year,season_dict[i]])
             count.append(np.sum(np.isfinite(data[mask])))
             dummy.append(np.nanmean(data[mask],axis=0))
             # dummy.append(np.nanmean(data[mask],axis=0) if count[-1]>=ratio else np.array([np.nan]*len(data[0])))
         return tummy,dummy,count
+    
+    @staticmethod
+    def _get_season(month):
+        '''
+        enum:
+            1: winter
+            2: spring
+            3: summer
+            4: autumn
+        '''
+        return (month%12+3)//3
 
     @staticmethod
     def _QC_numbers(data,count,threshold):
@@ -320,6 +377,45 @@ class Timean(object):
             else:
                 return tummy,dummy,count
     
+    def season(self,ratio=None,base=3):
+        '''
+        Spring: March, April, May
+        Summer: June, July, August
+        Autumn: September, October, November 
+        Winter: December, January, February
+        '''
+        if ratio!=None:
+            ratio=int(base*ratio) if ratio<=1 else int(ratio)
+        else:
+            if self.ratio!=None:
+                ratio=int(base*self.ratio) if self.ratio<=1 else int(self.ratio)
+
+        if self.config['fixTime']:
+            if self.config['zeroStart']:
+                tummy,dummy,count = self._fixTime(self.time,self.data,timeStep=dict(season=1),
+                    zeroPara=dict(day=1,hour=0,minute=0,second=0,microsecond=0),
+                    storageForward=self.config['storageForward'], starttime=self.starttime,endtime=self.endtime)
+            else:
+                tummy,dummy,count = self._fixTime(self.time,self.data,timeStep=dict(season=1),
+                    starttime=self.starttime,endtime=self.endtime)
+        else: # self.config['fixTime']==False
+            tummy,dummy,count = self._nofixTime(self.time,self.data,parameter='season')
+
+        dummy = self._QC_numbers(dummy,count,ratio)
+
+        if self.config['selfUpdate']:
+            self.data = np.array(dummy)
+            self.time = np.array(tummy)
+            self.counts = np.array(count)
+        else:
+            print("This is not object standard operation!")
+            print("You need to set config[selfUpdate]=True and use get method to get the result.")
+            dummy = self._set_header(dummy)
+            if self.config['asDict']:
+                return dict(time=tummy,data=dummy,counts=count)
+            else:
+                return tummy,dummy,count
+
     def year(self,ratio=None,base=12):
         if ratio!=None:
             ratio=int(base*ratio) if ratio<=1 else int(ratio)
@@ -368,13 +464,14 @@ if __name__ == "__main__":
     # Input data
     import datetime, random
     st = datetime.datetime(2020,1,1)
-    number = 1000
-    time = [st+datetime.timedelta(minutes=val) for val in range(number)]
+    number = 50000
+    time = [st+datetime.timedelta(hours=val) for val in range(number)]
     data = [[random.gauss(10,5) for _ in range(4)] for _ in range(number)]
     myobj.input(time,data,header=['a','b','c','d'])
 
     # Calculate and Get result
-    myobj.hour(1,500)
+    # myobj.hour(1,500)
+    myobj.season()
     myobj.set_config(asDict=True)
     result = myobj.get()
     print(result)
